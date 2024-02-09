cf952967a5641c07b0b29135026c75e073497638
nazarejose <josenazare1234@gmail.com>
2024-02-09 18:06:19 -0300

    first commit of a note creator

---
+++ src/app.tsx
@@ -0,0 +1,66 @@
+import { useState } from 'react'
+import logo from './assets/logo-nlw-expert.svg'
+import { NewNoteCard } from './components/new-note-card'
+import { NoteCard } from './components/note-card'
+
+interface Note{
+  id: string,
+  date: Date,
+  content: string
+}
+
+export function App() {
+
+  const [notes, setNotes] = useState<Note[]>(() => {
+    const notesOnStorage = localStorage.getItem('notas')
+
+    if(notesOnStorage){
+      return JSON.parse(notesOnStorage)
+    }
+
+    return[]
+  })
+
+  function onNoteCreated(content: string){
+    const newNote = {
+      id: crypto.randomUUID(),
+      date: new Date(),
+      content,
+    }
+
+    const notesArray = [newNote, ...notes]
+
+    setNotes(notesArray)
+
+    localStorage.setItem('notas', JSON.stringify(notesArray))
+  }
+  
+  return (
+    <div className='mx-auto max-w-6xl my-12 space-y-6'>
+      <img src={logo} alt="NLW Logo" />
+      <form className='w-full'>
+        <input 
+          type="text" 
+          placeholder='Busque em suas notas...'
+          className='w-full bg-transparent text-3xl font-semibold tracking-tight outline-none placeholder:text-slate-500'
+        />
+      </form>
+
+      <div className='h-px bg-slate-700'/>
+
+      <div className='grid grid-cols-3 gap-6 auto-rows-[250px]'>
+
+        <NewNoteCard onNoteCreated={onNoteCreated} /> 
+        
+        {notes.map(note => {
+          return <NoteCard key={note.id} note={note} />
+        })}
+        
+      </div>
+
+    </div>
+  )
+  
+}
+
+
